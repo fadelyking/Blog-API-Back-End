f82311c12f9a117abdfea56b79bfac027b474e2a
fadelyking <fadely195@gmail.com>
2024-01-14 12:01:20 +0300

    added comments router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 const mongoose = require("mongoose");
 const postsRouter = require("./routes/post");
 const usersRouter = require("./routes/user");
+const commentRouter = require("./routes/comment");
 const mongoDB = process.env.MONGODB_URI;
 main().catch((err) => console.log(err));
 async function main() {
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", postsRouter);
+app.use("/", commentRouter);
 app.use("/user", usersRouter);
 
 // catch 404 and forward to error handler
