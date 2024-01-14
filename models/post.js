db097738479a7678d7e6bd25f04d284f719327e9
fadelyking <fadely195@gmail.com>
2024-01-14 05:52:48 +0300

    added title and likes to posts

--- models/post.js
+++ models/post.js
@@ -3,7 +3,9 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
 	user: { type: Schema.Types.ObjectId, ref: "User" },
+	title: String,
 	content: String,
+	likes: Number,
 	comments: { type: Schema.Types.ObjectId, ref: "Comment" },
 	published: Boolean,
 });
