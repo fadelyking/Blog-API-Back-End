833c6da4c80890c2e08d18acf407aa17beb30bf0
fadelyking <fadely195@gmail.com>
2024-01-14 07:04:46 +0300

    created post routes

--- routes/post.js
+++ routes/post.js
@@ -3,5 +3,12 @@ const router = express.Router();
 const post_controller = require("../controllers/post");
 
 router.get("/", post_controller.index);
+router.get("/post/create", post_controller.create_post_get);
+router.post("/post/create", post_controller.create_post_post);
+router.get("/post/:id", post_controller.post_detail);
+router.get("/post/:id/delete", post_controller.delete_post_get);
+router.post("/post/:id/delete", post_controller.delete_posts_post);
+router.get("/post/:id/update", post_controller.update_post_get);
+router.post("/post/:id/update", post_controller.update_post_post);
 
 module.exports = router;
