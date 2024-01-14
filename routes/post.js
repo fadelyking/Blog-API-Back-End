f0a0432ff16096faa3b2d79d3ccdbd0aafcf2dba
fadelyking <fadely195@gmail.com>
2024-01-14 12:04:00 +0300

    added authorization to post routes

--- routes/post.js
+++ routes/post.js
@@ -7,9 +7,9 @@ router.get("/", post_controller.index);
 router.get("/post/create", auth, post_controller.create_post_get);
 router.post("/post/create", auth, post_controller.create_post_post);
 router.get("/post/:id", post_controller.post_detail);
-router.get("/post/:id/delete", post_controller.delete_post_get);
-router.post("/post/:id/delete", post_controller.delete_posts_post);
-router.get("/post/:id/update", post_controller.update_post_get);
-router.post("/post/:id/update", post_controller.update_post_post);
+router.get("/post/:id/delete", auth, post_controller.delete_post_get);
+router.post("/post/:id/delete", auth, post_controller.delete_posts_post);
+router.get("/post/:id/update", auth, post_controller.update_post_get);
+router.post("/post/:id/update", auth, post_controller.update_post_post);
 
 module.exports = router;
