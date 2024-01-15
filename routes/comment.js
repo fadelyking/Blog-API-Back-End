a9666731d9f62dbc8f7f6b9725ae75b893622bf7
fadelyking <fadely195@gmail.com>
2024-01-15 03:54:20 +0300

    routes to comments

--- routes/comment.js
+++ routes/comment.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const auth = require("../config/verifyToken");
 const comments_controller = require("../controllers/comment");
 
-router.get("/post/:id", comments_controller.list_comments_get);
-router.post("/post/:id", auth, comments_controller.create_comment_post);
-router.post("/post/:id", auth, comments_controller.delete_comment_post);
-
+router.get("/post/:id/comment", comments_controller.list_comments_get);
+router.post("/post/:id/comment", auth, comments_controller.create_comment_post);
+router.post("/post/:id/comment", auth, comments_controller.delete_comment_post);
 module.exports = router;
