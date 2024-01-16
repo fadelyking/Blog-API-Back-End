406fc6decaa78c9bc89230914f89d6850de3f7fd
fadelyking <fadely195@gmail.com>
2024-01-16 14:11:11 +0300

    fetching all users on login

--- controllers/user.js
+++ controllers/user.js
@@ -7,75 +7,85 @@ const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
-exports.create_users_get = (req, res, next) => {
-	res.json({ message: "This route works" });
-};
-
+exports.create_users_get = asyncHandler(async (req, res, next) => {
+  const user = User.find().populate("posts").exec();
+  res.json({ user: user });
+});
 exports.create_users_post = [
-	body("first_name", "Must be more than 1 letter").trim().isLength({ min: 1 }).escape(),
-	body("last_name", "Must be more than 1 letter").trim().isLength({ min: 1 }).escape(),
-	body("email", "Please use correct email form").trim().isEmail().escape(),
-	body("password", "Password must be more than 6 characters")
-		.trim()
-		.isLength({ min: 6 })
-		.escape(),
-	body("confirm_password", "Passwords are not matching")
-		.trim()
-		.custom((value, { req }) => {
-			return value === req.body.password;
-		}),
+  body("first_name", "Must be more than 1 letter")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("last_name", "Must be more than 1 letter")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("email", "Please use correct email form").trim().isEmail().escape(),
+  body("password", "Password must be more than 6 characters")
+    .trim()
+    .isLength({ min: 6 })
+    .escape(),
+  body("confirm_password", "Passwords are not matching")
+    .trim()
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    }),
 
-	asyncHandler(async (req, res, next) => {
-		const errors = validationResult(req);
-		console.log(req);
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    console.log(req);
 
-		if (!errors.isEmpty()) {
-			console.log(errors);
-			res.json({ error: errors.array() });
-		} else {
-			bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-				const user = new User({
-					first_name: req.body.first_name,
-					last_name: req.body.last_name,
-					email: req.body.email,
-					password: hashedPassword,
-					admin: false,
-				});
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      res.json({ error: errors.array() });
+    } else {
+      bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+        const user = new User({
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          email: req.body.email,
+          password: hashedPassword,
+          admin: false,
+        });
 
-				await user.save();
-				res.status(200).json({ message: "User Created" });
-			});
-		}
-	}),
+        await user.save();
+        res.status(200).json({ message: "User Created" });
+      });
+    }
+  }),
 ];
 
 exports.login_get = (req, res, next) => {
-	res.json({});
+  res.json({});
 };
 
 exports.login_post = [
-	body("email", "Please use correct email form").trim().isEmail().escape(),
-	body("password", "Password must be more than 6 characters")
-		.trim()
-		.isLength({ min: 6 })
-		.escape(),
+  body("email", "Please use correct email form").trim().isEmail().escape(),
+  body("password", "Password must be more than 6 characters")
+    .trim()
+    .isLength({ min: 6 })
+    .escape(),
 
-	asyncHandler(async (req, res, next) => {
-		console.log(req.body.email);
-		console.log(req.body.password);
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			res.status(403).res.json({ error: errors.msg });
-		}
-		const user = await User.findOne({ email: req.body.email });
-		const match = await bcrypt.compare(req.body.password, user.password);
-		console.log(match);
-		const accessToken = jwt.sign(JSON.stringify(user), process.env.TOKEN_SECRET);
+  asyncHandler(async (req, res, next) => {
+    console.log(req.body.email);
+    console.log(req.body.password);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      res.status(403).res.json({ error: errors });
+    }
+    const user = await User.findOne({ email: req.body.email });
+    const match = await bcrypt.compare(req.body.password, user.password);
+    console.log(match);
+    const accessToken = jwt.sign(
+      JSON.stringify(user),
+      process.env.TOKEN_SECRET
+    );
 
-		if (match) {
-			res.json({ accessToken });
-		} else {
-			res.json({ message: "invalid info" });
-		}
-	}),
+    if (match) {
+      res.json({ accessToken });
+    } else {
+      res.json({ message: "invalid info" });
+    }
+  }),
 ];
